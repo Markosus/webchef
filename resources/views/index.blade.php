c63f4fadf02458595381363e43643674e8086ca9
Markosus <mark2002david@hotmail.com>
2016-09-30 13:47:06 -0400

    Changed the main.css and the index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,118 @@
 
 @section('content')
 
+<div id="about">
+   	<div class="container">
+
+   		<div class="row">
+
+   			<div class="col-md-12">
+
+   				<h1>
+   					 web development, <span style="color:#800f13;">hosting</span>, and <span style="color:#400709;">projects</span>
+   				</h1>
+
+   				<br>
+   				<br>
+
+   				<h2>about</h2>	
+   				<br>	
+   				<!-- <h3 class="text-center">A little bit about myself</h3> -->
+   				<br>
+   				<p class="text-center">
+   					Im a independant, freelance web developer. My experience is primarily with back-end development. I do dive into the front-end of web design here and there using Adobe Photoshop and Illustrator.
+   				</p>
+
+   			</div>
+
+   			
 
 
+   			
+   		</div> <!-- end row -->
+
+ 	</div> <!-- end container -->
+
+ 	<br>
+   	<br>
+
+ 	<div class="containter-fluid">
+ 		
+ 		
+
+ 			<div class="left col-md-6">
+ 				
+ 			</div>
+
+ 			<div class="right col-md-6">
+
+ 				<div class="innerright">
+
+	 				<h3 style="color:white;">Experience and Knowledge</h3>
+	 				<br>
+
+	 				
+
+	 				<table>
+	 					<tr>
+	 						<td>
+	 							<ul style="color:white;">
+
+				 					<li>Object Oriented PHP</li>
+				 					<li>Laravel Frame Work</li>
+				 					<li>Adobe Photoshop</li>
+				 					<li>Adobe Illustrator</li>
+				 					<li>MySQL</li>
+
+				 				</ul>
+	 						</td>
+							<td>
+								<ul style="color:white;">
+
+				 					<li>WordPress</li>
+				 					<li>Twitter Bootstrap</li>
+				 					<li>HTML5</li>
+				 					<li>JQuery</li>
+
+				 				</ul>
+				 			</td>	
+
+	 					</tr>
+	 					
+	 				</table>
+
+	 				
+
+	 				
+	 				
+
+
+	 			</div>
+ 				
+ 			</div>
+ 			
+ 		
+
+ 	</div> <!-- end fluid container -->
+
+<div class="container">
+
+   		<div class="row">
+
+   			<div class="col-md-12">
+
+   				<br>
+   			
+   				<p class="text-center">
+   					I would like to think my greatest assests in web development are my passion to learn new technologies, satisfaction from doing good work and stubborness when troubleshooting any obstacles.
+   					<br><br>
+					Did you need any work done? Contact me.</p>
+
+   			</div>
+
+   		</div> <!-- end  row -->
+
+</div><!-- end  container -->
+
+</div> <!-- end about -->
 @endsection
