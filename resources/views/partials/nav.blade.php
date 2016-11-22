0e12b09a8bd0ed426b355b19d4c215c29f37fb3a
Markosus <mark2002david@hotmail.com>
2016-11-22 12:08:07 -0500

    changed menu dropdown postion

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -14,7 +14,7 @@
 <!-- Collect the nav links, forms, and other content for toggling -->
 
 <div class="collapse navbar-collapse navbar-background" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav navbar-right ">
+    <ul class="nav navbar-nav navbar-right">
          <li{!! (Request::is('/')) ? ' class="active"' : '' !!}>
             <a href="{{ URL::to('/')}}" >About Author </a>
         </li>
